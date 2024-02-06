90a45d19c5321eed57317801c4589fb4ccb09209
Jamshidbek <kamoliddinovjamshid7@gmail.com>
2024-02-06 20:14:42 +0500

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      sans: ['Roboto', 'sans-serif'],
+      serif: ['Playfair Display', 'serif'],
+    }
+  },
+  plugins: [],
+};
+
